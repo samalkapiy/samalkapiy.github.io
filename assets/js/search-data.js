86222048ccde691de12632b2cf31c54883e5f562
samalkapiy <35342088+samalkapiy@users.noreply.github.com>
2026-05-18 19:19:09 +0000

    Deploying to gh-pages from @ samalkapiy/samalkapiy.github.io@a063b308d3c7aa3d73028cc8eb63d91c9fef5178 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,9 +424,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-credit-card-fraud-detection-with-xgboost",
+          title: 'Credit card fraud detection with XGBoost',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
