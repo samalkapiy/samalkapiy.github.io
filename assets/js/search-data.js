55392233f8ce4ac2ad62ab6daa43425d63eac277
samalkapiy <35342088+samalkapiy@users.noreply.github.com>
2026-05-22 19:21:55 +0000

    Deploying to gh-pages from @ samalkapiy/samalkapiy.github.io@e51c1cbdf885c1b15d3c35d14a2f1223a7dbc3f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,11 +419,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
