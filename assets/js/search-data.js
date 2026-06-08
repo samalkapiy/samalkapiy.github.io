4d56c8d8c87b5671b8671b3098c4b6d92ae3bcdc
samalkapiy <35342088+samalkapiy@users.noreply.github.com>
2026-06-08 19:04:04 +0000

    Deploying to gh-pages from @ samalkapiy/samalkapiy.github.io@ac6ad9423620d20a5df442e93fb70d1bbdac810a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/Samalka_Anandagoda_v2.pdf";
+            window.location.href = "/assets/pdf/Samalka_Anandagoda_CV.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
