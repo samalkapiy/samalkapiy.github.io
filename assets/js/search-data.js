7593663226b09b3995a3733a7c58ae2bac147515
samalkapiy <35342088+samalkapiy@users.noreply.github.com>
2026-06-15 22:01:01 +0000

    Deploying to gh-pages from @ samalkapiy/samalkapiy.github.io@7952235cf6e94dd9b276a7ee1517e2f33af0bbb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,12 +409,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-spacex-launch-success-prediction",
+          section: "News",},{id: "projects-spacex-launch-success-prediction",
           title: 'SpaceX launch success prediction',
           description: "Model built using XGBoost (Decision Trees, Hyperparameter tuning, Cross validation, cost estimation). IBM professional certificate project",
           section: "Projects",handler: () => {
@@ -424,6 +419,11 @@ ninja.data = [{
           description: "model built to predict fraudulent transactions and estimate the monthly net savings. (XGBoost, ROC-AUC score, cross validation)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/credit_card_fraud/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/tableau_telco_churn/";
             },},{
         id: 'social-email',
         title: 'email',
